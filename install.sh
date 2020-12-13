7b72868f4283e4ed1532feea5dd0780859afedeb
inbox715 <43434574+inbox715@users.noreply.github.com>
2020-12-13 08:57:41 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,5 +11,5 @@ sudo apt install git
 sudo rm -rf openvpn-user
 git clone https://github.com/inbox715/openvpn-user 
 
-nohup python3 ./openvpn-user/user.py &
-nohup python3 ./openvpn-user/server.py &
+python3 ./openvpn-user/user.py &
+python3 ./openvpn-user/server.py &
